41b6c2d97f13d99a69f09ae9e5c59715c5b206de
Prabhat Sharma <hi.prabhat@gmail.com>
2022-04-26 23:12:02 -0700

    comment

--- coverage.sh
+++ coverage.sh
@@ -2,6 +2,7 @@
 
 go test ./... -race -covermode=atomic -coverprofile=coverage.out
 
+# make sure to set CODECOV_TOKEN env variable before doing this
 codecov -f coverage.out
 # or 
 # bash <(curl -s https://codecov.io/bash) 
